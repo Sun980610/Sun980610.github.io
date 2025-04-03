3f25d073426271216111f29da5b42ce33dd3c558
Sun980610 <Sun980610@users.noreply.github.com>
2025-04-03 03:28:30 +0000

    deploy: 7017d8eb51cf24f84cfa7a6505f7bff824cd236c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -48,7 +48,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Server User",
-        "excerpt":"                외부에서 서버 접근 불가                    본체 부팅 오류                    명령어 작동 문제                    네트워크 오류                    파일 접근 권한 문제                    프로세스 중단 문제            ","categories": [],
+        "excerpt":"                외부에서 서버 접근 불가                    본체 부팅 오류                    명령어 작동 문제                    네트워크 오류                    파일 접근 권한 문제                    프로세스 중단 문제          ","categories": [],
         "tags": [],
         "url": "/mi/server/user/",
         "teaser": null
