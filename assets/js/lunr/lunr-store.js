713a77d0ee0a0a3c9525ff28f09af5b5bf23ceda
Sun980610 <Sun980610@users.noreply.github.com>
2025-04-03 03:40:30 +0000

    deploy: 769f1fc3fa8e730e63f0a7bab5b4f7e8c5a8222a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -90,7 +90,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Statistics Metabolites R",
-        "excerpt":"                외부에서 서버 접근 불가                    본체 부팅 오류                    명령어 작동 문제                    네트워크 오류                    파일 접근 권한 문제                    프로세스 중단 문제          ","categories": [],
+        "excerpt":"      ","categories": [],
         "tags": [],
         "url": "/mi/statistics/metabolitesR/",
         "teaser": null
