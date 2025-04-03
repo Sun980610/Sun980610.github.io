781975bb0df2e5c6ec00fc8554f23a52febe1fe4
Sun980610 <Sun980610@users.noreply.github.com>
2025-04-03 03:30:49 +0000

    deploy: 348ea490a3a67b230e8455daa93ac3ba2c977e66

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -11,6 +11,12 @@ var store = [{
         "url": "/mi/informatics/microbiome/",
         "teaser": null
       },{
+        "title": "Multiomics Informatical Process",
+        "excerpt":"                외부에서 서버 접근 불가                    본체 부팅 오류                    명령어 작동 문제                    네트워크 오류                    파일 접근 권한 문제                    프로세스 중단 문제          ","categories": [],
+        "tags": [],
+        "url": "/mi/informatics/multiomics/",
+        "teaser": null
+      },{
         "title": "Server Setup",
         "excerpt":"                외부에서 서버 접근 불가                    본체 부팅 오류                    명령어 작동 문제                    네트워크 오류                    파일 접근 권한 문제                    프로세스 중단 문제          ","categories": [],
         "tags": [],
