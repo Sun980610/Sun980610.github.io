a833719901b833110743421c1cce98bb7759f038
Sun980610 <Sun980610@users.noreply.github.com>
2025-04-04 08:59:25 +0000

    deploy: a076e1be25f54bd103ad5bf9ff1159566cf05746

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -30,7 +30,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Server Setup",
-        "excerpt":"                04. SSH 설치                    02. Conda 설치                    03. 포트 설정 방법                    01. Ubuntu 설치          ","categories": [],
+        "excerpt":"      ","categories": [],
         "tags": [],
         "url": "/mi/server/setup/",
         "teaser": null
